0458f834543807116d6c15f3815de5d2d437b12e
Leleat <atule@pm.me>
2025-01-04 14:41:31 +0100

    chore: add Injections utility singleton
    This commit adds a singleton to manage custom properties and methods
    injected into GNOME's existing objects. The singleton is used to track
    and remove injected properties and methods when the extension is disabled.
    
    Part-of: https://github.com/Leleat/floating-scroll/pull/6

--- src/extension.js
+++ src/extension.js
@@ -1,9 +1,18 @@
 import { Extension } from "./shell/dependencies.js";
 
+import {
+    disable as disableInjections,
+    enable as enableInjections,
+} from "./shell/utils/injections.js";
+
 export default class FloatingScroll extends Extension {
     enable() {
+        // singletons
+        enableInjections();
     }
 
     disable() {
+        // singletons
+        disableInjections();
     }
 }
